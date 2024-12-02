b5a16cf78b2945229c8ad9aecc43365f894dde6e
Alaa Batayneh <92776597+6u5haman@users.noreply.github.com>
2024-12-01 18:00:50 -0600

    up

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,12 @@ const HomePage = () => {
   return (
     <section className="container">
       <div className="mt-5">
+        <p>
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer congue, libero sit amet interdum consequat, orci nibh interdum lorem, nec vehicula nisl libero in quam. Nam cursus, leo sed efficitur sagittis, est erat lacinia odio, at vehicula lacus tortor a nulla. Vivamus et nulla id ipsum consequat malesuada. Nullam ac magna vitae lorem gravida mollis id non lacus. Quisque pharetra purus sed mi consectetur, sit amet scelerisque magna faucibus.
+        </p>
+        <p>
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer congue, libero sit amet interdum consequat, orci nibh interdum lorem, nec vehicula nisl libero in quam. Nam cursus, leo sed efficitur sagittis, est erat lacinia odio, at vehicula lacus tortor a nulla. Vivamus et nulla id ipsum consequat malesuada. Nullam ac magna vitae lorem gravida mollis id non lacus. Quisque pharetra purus sed mi consectetur, sit amet scelerisque magna faucibus.
+        </p>
       </div>
     </section>
   )
